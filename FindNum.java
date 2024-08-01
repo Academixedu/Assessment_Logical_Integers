90b1f31abdfe05764dc6a7bfd89359362f1a5d35
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-08-01 17:31:29 +0530

    From Karthik

--- FindNum.java
+++ FindNum.java
@@ -1,6 +1,9 @@
-public FindNum{
-// Prepare a Logic to find out 2nd  number in a set of number 
-  // Let's Take a Number in Scanner
-  // Example if you take 235 as number it should return 3
-  // Likewise Logic you have written should get 2nd digit of any number
+import java.util.Scanner;
+public class FindNum{
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a Number:");
+      String strnum=sc.nextLine();
+      System.out.println("The Second Digit is:  "+strnum.charAt(1));
+    }
 }
