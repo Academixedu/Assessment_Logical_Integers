91c3da56af6a08b8e773ae2afbf73bc7d8c19dc0
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-08-01 16:11:35 +0530

    java

--- FindOdd.java
+++ FindOdd.java
@@ -1,4 +1,16 @@
 public class FindOdd{
+  public static void main(String[] args) {
+    String m="233466";
+    for(int i=1;i<=m.length();i++){
+      int num=Character.getNumericValue(m.charAt(i-1));
+      if(i%2!=0){
+        System.out.println(num);
+      }
+
+
+    }
+    
+  }
 // Prepare a Logic to print odd didgit in anumber
   // Example if we have taken Number 122456 it should return digits which are in odd position in this number it should retrun odd placed values 
   // 1 
